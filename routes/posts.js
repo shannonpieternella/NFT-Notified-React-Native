cb2b1a545572b5fcf96c91fdd50e38a5a5b2e65a
Shannon Pieternella <shannonpieternella@gmail.com>
2021-10-21 18:59:17 +0200

    my first commit

--- routes/posts.js
+++ routes/posts.js
@@ -100,7 +100,7 @@ const doSomethingMain = async () => {
         console.log('scraped weblink output');
         console.log('scraped:', articles[2]);
     
-        
+        doSomething();
         //Put here
         const doSomething = async () => {
             console.log('started checking condition with db data and scraped data. both data should be available now');
@@ -213,7 +213,7 @@ const doSomethingMain = async () => {
             
         }}
       
-          doSomething();
+          
 
           
            
