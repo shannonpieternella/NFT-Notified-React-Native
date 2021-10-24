3a5649c11dca0f975344c504241878136999f340
Shannon Pieternella <shannonpieternella@gmail.com>
2021-10-24 03:11:11 +0200

    my first commit

--- routes/posts.js
+++ routes/posts.js
@@ -207,7 +207,7 @@ console.log('loop start nr', i, 'status db count:', countstatus);
                         <!-- Set text color to background color -->
                         <div style="display: none; visibility: hidden; overflow: hidden; opacity: 0; font-size: 1px; line-height: 1px; height: 0; max-height: 0; max-width: 0;
                         color: #F0F0F0;" class="preheader">
-                            Available on&nbsp;GitHub and&nbsp;CodePen. Highly compatible. Designer friendly. More than 50%&nbsp;of&nbsp;total email opens occurred on&nbsp;a&nbsp;mobile device&nbsp;— a&nbsp;mobile-friendly design is&nbsp;a&nbsp;must for&nbsp;email campaigns.</div>
+                            </div>
             
                         <!-- LOGO -->
                         <!-- Image text color should be opposite to background color. Set your url, image src, alt and title. Alt text should fit the image size. Real image size should be x2. URL format: http://domain.com/?utm_source={{Campaign-Source}}&utm_medium=email&utm_content=logo&utm_campaign={{Campaign-Name}} -->
