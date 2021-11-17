cdb1aa28b5e4b57af4205829efa45a11429dc284
Shannon Pieternella <shannonpieternella@gmail.com>
2021-11-17 15:41:12 +0100

    my first commit

--- routes/posts.js
+++ routes/posts.js
@@ -280,7 +280,7 @@ while (i < countCollectionsOfficialDb) {
     const userId = await collectiesUser[x]._id.toString();
     const collectionUserStatus = await collectiesUser[x].status.toString();
     const pushkey = await collectiesUser[x].pushkey.toString();
-    const ethuserinp = await collectiesUser[x].eth_userinput.toString();
+    // const ethuserinp = await collectiesUser[x].eth_userinput.toString();
     const name = await collectiesUser[x].name_collection.toString();
 
 
