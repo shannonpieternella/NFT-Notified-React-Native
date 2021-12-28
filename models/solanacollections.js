86792b194fe92a3330dfd48ef431e9516272fd30
Shannon Pieternella <shannonpieternella@gmail.com>
2021-12-28 17:06:05 +0100

    my first commit

---
+++ models/solanacollections.js
@@ -0,0 +1,22 @@
+const { Decimal128 } = require('mongodb');
+const mongoose = require('mongoose');
+
+const PostCollections = mongoose.Schema({
+
+collection_link: {
+    type: String,
+    required:true
+    },
+    floorprice: {
+        type: Decimal128,
+        required:true
+        },
+            collection_name: {
+                type: String,
+                required:true
+                }
+            
+
+});
+
+module.exports = mongoose.model('solanacollections', PostCollections);
