3165248f1802a5e21a9b5d24f91dc336822b339c
Shannon Pieternella <shannonpieternella@gmail.com>
2022-01-08 00:42:51 +0100

    my first commit

--- routes/posts.js
+++ routes/posts.js
@@ -157,6 +157,26 @@ router.get('/collectiequery/:search', async (req, res) => {
         }
 });
 
+//Get back all collections queries
+router.get('/solanart/:search', async (req, res) => {
+    try {
+       const searchnow = req.params.search;
+
+       if(searchnow == ""){
+        const allPosts = await solanacollections.find();;
+        res.json(allPosts);
+
+      
+       }else{
+        const posts = await solanacollections.find({collection_name: {$regex:searchnow,$options: 'i'}});;
+        res.json(posts);
+       }
+
+    } catch (err) {
+        res.json({ message: err });
+        }
+});
+
 //Get back all the post
 router.get('/delete', async (req, res) => {
     try {
