4935b18fd148e924a1ce174c23e0f4c4ab5479a0
Shannon Pieternella <shannonpieternella@gmail.com>
2022-01-11 17:10:11 +0100

    make it better

--- routes/posts.js
+++ routes/posts.js
@@ -335,12 +335,12 @@ while (i < countCollectionsOfficialDb) {
     const collectionWebUrl = await collectiesWeb[i].collection_link.toString();
     const collectionDbPrice = await collectiesWeb[i].floorprice;
     const collectionName = await collectiesWeb[i].collection_name.toString();
-    console.log("count of all posts: ", i);
+    console.log("count of all posts opensea: ", i);
 
     const collectiesUserCount = await Post.count({status:0, collection_link:collectionWebUrl});
     for (let x = 0; x < collectiesUserCount; x++) {
         //User form db gegevens
-        console.log("count of inside posts: ", x); 
+        console.log("count of inside posts opensea: ", x); 
     const collectiesUser = await Post.find({status:0, collection_link:collectionWebUrl});    
     const emailId = await collectiesUser[x].email_id.toString();
     const collectionUrlUser = await collectiesUser[x].collection_link.toString();
@@ -1031,12 +1031,12 @@ while (i < countCollectionsOfficialDb) {
     const collectionWebUrl = await collectiesWeb[i].collection_link.toString();
     const collectionDbPrice = await collectiesWeb[i].floorprice;
     const collectionName = await collectiesWeb[i].collection_name.toString();
-    console.log("count of all posts: ", i);
+    console.log("count of all posts immutablex: ", i);
 
     const collectiesUserCount = await Post.count({status:0, collection_link:collectionWebUrl});
     for (let x = 0; x < collectiesUserCount; x++) {
         //User form db gegevens
-        console.log("count of inside posts: ", x); 
+        console.log("count of inside posts immutablex: ", x); 
     const collectiesUser = await Post.find({status:0, collection_link:collectionWebUrl});    
     const emailId = await collectiesUser[x].email_id.toString();
     const collectionUrlUser = await collectiesUser[x].collection_link.toString();
